fc478c52c68fc75b28338290aee779992956cd48
Oguzhan Akan <oakan13@ku.edu.tr>
2022-03-26 02:57:27 -0700

    homepage first iteration

--- lib/variables/urls.dart
+++ lib/variables/urls.dart
@@ -3,3 +3,4 @@ const String UPDATE_USER_URL = "/user/update";
 const String GET_LOCATIONS_URL = "/api/v1/locations/";
 const String GET_BLACKOUT_SLOTS_URL = "/api/v1/locations/blackout-slots/";
 const String MAKE_APPOINTMENT_URL = "/api/v1/labtest/make-appointment/";
+const String GET_APPOINTMENTS_URL = "/api/v1/users/appointments/";
