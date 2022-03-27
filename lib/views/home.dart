66ccd37232c5803495d11c751348cf97344741e5
Oguzhan Akan <oakan13@ku.edu.tr>
2022-03-26 22:13:57 -0700

    homepage now showing appointments from the database

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -5,6 +5,7 @@ import 'package:covidlab/services/loginmethods.dart';
 import 'package:covidlab/services/requests.dart';
 import 'package:covidlab/variables/urls.dart';
 import 'package:covidlab/widgets/add.dart';
+import 'package:covidlab/widgets/appointmentCard.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
@@ -21,8 +22,8 @@ class _HomeState extends State<Home> {
   bool? _loading = true;
   bool? _loadingError = false;
   UserRepository? userRepository;
-  List<dynamic> upcomingAppointments = [];
-  List<dynamic> pastAppointments = [];
+  // List<dynamic> upcomingAppointments = [];
+  // List<dynamic> pastAppointments = [];
 
   void initState() {
     super.initState();
@@ -42,7 +43,7 @@ class _HomeState extends State<Home> {
             icon: Icon(Icons.add),
             onPressed: () async {
               {
-                bool? refresh = await showModalBottomSheet<bool>(
+                await showModalBottomSheet<bool>(
                   useRootNavigator: true,
                   shape: RoundedRectangleBorder(
                       borderRadius:
@@ -53,11 +54,7 @@ class _HomeState extends State<Home> {
                     return FractionallySizedBox(
                         heightFactor: 0.88, child: AddAppointment());
                   },
-                );
-                if (refresh!)
-                  setState(() {
-                    _loading = true;
-                  });
+                ).then((value) => setState(() {}));
               }
             },
           ),
@@ -84,145 +81,14 @@ class _HomeState extends State<Home> {
                     child: Column(children: [
                       Column(
                           children: List.generate(
-                        upcomingAppointments.length,
+                        userRepository!.appointments.length,
                         (index) {
-                          return upcomingAppointmentWidget(
-                              upcomingAppointments[index]);
+                          return AppointmentCard(
+                              userRepository!.appointments[index]);
                         },
                       )),
-                      Divider(
-                        height: 50,
-                        thickness: 1,
-                        indent: 0,
-                        endIndent: 0,
-                        color: CupertinoColors.systemGrey2,
-                      ),
-                      Column(
-                          children: List.generate(
-                        pastAppointments.length,
-                        (index) {
-                          return upcomingAppointmentWidget(
-                              upcomingAppointments[index]);
-                        },
-                      ))
                     ])),
-                // child: ListView(children: [
-                //   Container(
-                //       padding: EdgeInsets.all(24.0),
-                //       child: Column(
-                //           crossAxisAlignment: CrossAxisAlignment.start,
-                //           children: [
-                //             const Divider(
-                //               height: 50,
-                //               thickness: 1,
-                //               indent: 0,
-                //               endIndent: 0,
-                //               color: CupertinoColors.systemGrey2,
-                //             ),
-                //             Container(
-                //               width: MediaQuery.of(context).size.width,
-                //               child: Card(
-                //                 child: InkWell(
-                //                   onTap: () {
-                //                     print('Card tapped.');
-                //                   },
-                //                   // mainAxisSize: MainAxisSize.min,
-                //                   child: Column(
-                //                     children: [
-                //                       ListTile(
-                //                         leading: Icon(
-                //                             Icons.access_time_filled_rounded),
-                //                         title: Text('Past Test'),
-                //                         subtitle: Text(
-                //                           '10/02/2022 09:30 AM',
-                //                           style: TextStyle(
-                //                               color: Colors.black
-                //                                   .withOpacity(0.6)),
-                //                         ),
-                //                       ),
-                //                       // Row(
-                //                       //   mainAxisAlignment: MainAxisAlignment.end,
-                //                       //   children: <Widget>[
-                //                       //     IconButton(
-                //                       //         icon: const Icon(
-                //                       //           CupertinoIcons.arrow_right_circle,
-                //                       //           color: Colors.black,
-                //                       //           size: 30,
-                //                       //         ),
-                //                       //         padding: const EdgeInsets.all(0),
-                //                       //         onPressed: () {}
-                //                       //     ),
-                //                       //     const SizedBox(width: 8),
-                //                       //   ],
-                //                       // ),
-                //                     ],
-                //                   ),
-                //                 ),
-                //               ),
-                //             ),
-                //             SizedBox(
-                //               height: 12.0,
-                //             ),
-                //           ]))
-                // ]),
-              ])
-                // Text('homepage'),
-                // onPressed: () {},
-                ));
-  }
-
-  Widget upcomingAppointmentWidget(dynamic appointment) {
-    return Container(
-        width: MediaQuery.of(context).size.width,
-        child: Card(
-          child: InkWell(
-            splashColor: Colors.blue.withAlpha(50),
-            onTap: () {
-              print('Card tapped.');
-            },
-            // shadowColor:Colors.grey,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ListTile(
-                  leading: Icon(Icons.access_time_rounded),
-                  title: Text(appointment["location"]),
-                  subtitle: Text(
-                    appointment["test_date"],
-                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ));
-  }
-
-  Widget pastAppointmentWidget(dynamic appointment) {
-    return Container(
-        width: MediaQuery.of(context).size.width,
-        child: Card(
-          child: InkWell(
-            splashColor: Colors.blue.withAlpha(50),
-            onTap: () {
-              print('Card tapped.');
-            },
-            // shadowColor:Colors.grey,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ListTile(
-                  leading: Icon(Icons.access_time_rounded),
-                  title: Text('Upcoming Appointment'),
-                  subtitle: Text(
-                    '12/23/2023 10:30 AM',
-                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ));
+              ])));
   }
 
   onAfterBuild(BuildContext context) async {
@@ -246,23 +112,16 @@ class _HomeState extends State<Home> {
 
       print("appointments:");
       print(allAppointments);
-      allAppointments.forEach((appointment) {
-        if (DateTime.parse(appointment["test_date"]).isAfter(DateTime.now())) {
-          upcomingAppointments.add(appointment);
-        } else {
-          pastAppointments.add(appointment);
-        }
-      });
+      print("userRepository!.appointments");
+      print(userRepository!.appointments);
 
-      upcomingAppointments
-          .sort((a, b) => a["test_date"].compareTo(b["test_date"]));
-      pastAppointments.sort((a, b) => a["test_date"].compareTo(b["test_date"]));
-
-      print("upcomingAppointments:");
-      print(upcomingAppointments);
-
-      print("pastAppointments:");
-      print(pastAppointments);
+      allAppointments.sort((a, b) => DateTime.parse(a["test_date"])
+              .isBefore(DateTime.parse(b["test_date"]))
+          ? 1
+          : 0);
+      userRepository!.appointments = allAppointments;
+      print("userRepository!.appointments:");
+      print(userRepository!.appointments);
 
       setState(() {
         _loadingError = false;
